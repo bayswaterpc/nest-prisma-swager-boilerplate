71118b7752a2279d97833689bbae51820686ee69
Jacob <bayswaterpc@gmail.com>
2022-07-29 15:20:23 -0400

    feat: add dummy jwt auth guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { ApiTags } from '@nestjs/swagger';
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -15,6 +21,13 @@ export class AppController {
   @Post('auth/login')
   @ApiTags('authorization')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @ApiTags('authorization')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
