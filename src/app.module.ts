a78f8c59af715d06634c7c8897868a30e4e68190
Jacob <bayswaterpc@gmail.com>
2022-07-29 14:15:24 -0400

    feat: add passportjs and generated user & auth items

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, TodosModule],
+  imports: [PrismaModule, ArticlesModule, TodosModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
