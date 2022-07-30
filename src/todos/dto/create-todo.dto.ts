77c93dc5549f2d81e978b72af7753df2a1cbb4bc
Jacob <bayswaterpc@gmail.com>
2022-07-29 21:36:06 -0400

    refactor: add global swagger compiler option, remove @ApiProperty calls

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,7 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTodoDto {
-  @ApiProperty()
   description: string;
 
   @ApiProperty({ required: false, default: false })
--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -4,16 +4,8 @@ import { ApiProperty } from '@nestjs/swagger';
 export class TodoEntity implements Todo {
   @ApiProperty()
   id: string;
-
-  @ApiProperty()
   description: string;
-
-  @ApiProperty()
   completed: boolean;
-
-  @ApiProperty()
   createdAt: Date;
-
-  @ApiProperty()
   updatedAt: Date;
 }
